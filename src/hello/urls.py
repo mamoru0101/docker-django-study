9bc5f4c462e72fb6e630dbbe8bc80b8de618d65b
kdm <d08020386730@gmail.com>
2021-04-10 22:14:00 +0900

    【 add 】テンプレートファイル追加

--- src/hello/urls.py
+++ src/hello/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
+
 from . import views
 
+app_name = "hello"
+
 urlpatterns = [
-    path("", views.index)
+    path("", views.index, name='top'),
+    path("hello/", views.hello, name='hello'),
 ]
--- src/hello/views.py
+++ src/hello/views.py
@@ -2,4 +2,8 @@ from django.shortcuts import render
 
 
 def index(request):
+    return render(request, template_name="index.html")
+
+
+def hello(request):
     return render(request, template_name="hello/index.html")
